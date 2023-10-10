6374dc53ddfaaf23fdaf9bc1afa2e94b5072f60c
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-10-10 16:32:05 +0100

    Lots of code tidy-up:
    - Renamed function `get_gmail_threads_response` to `get_gmail_threads_list`.
    - Extracted `parse_thread` helper function.
    - Extracted code for configuring Gmail service to a new function `get_gmail_service`.
    - Extracted code for getting list of threads from Gmail to a new function `get_gmail_thread_ids`.
    - Extracted code for getting a specific thread from Gmail to a new function `get_gmail_thread`.
    - Extracted helper function `decode_bytes` for decoding base64 encoded data.
    - Extracted helper function `body_has_data` for checking if message body contains data.
    - Extracted helper function `is_container_mime_message_part` for checking if message part is a container MIME type.
    - Extracted helper function `decode_body` for decoding the body of a specific message part.
    - Extracted helper function `parse_message_part` for parsing individual message parts.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import base64
 import os
 import openai
@@ -7,32 +9,26 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from stubs.gmail import GmailThreadsListResponse
+from stubs.gmail import GmailThread
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+# #### ##
+# OPEN AI
+# #### ##
 
 # Configure openai
 openai.organization = os.getenv('OPENAI_ORG_ID')
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
-def get_gmail_threads_response(response: dict) -> GmailThreadsListResponse:
-    return GmailThreadsListResponse(
-        threads=response.get('threads'),
-        next_page_token=response.get('nextPageToken'),
-        result_estimate_size=response.get('resultEstimateSize'),
-    )
+# #####
+# GMAIL
+# #####
 
-
-def decode_bytes(data):
-    try:
-        return base64.urlsafe_b64decode(data).decode('utf-8')
-    except Exception as e:
-        print(f"An error occurred while decoding: {e}")
-
-
-def main():
+# Configure Gmail service
+def get_gmail_service():
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -51,33 +47,86 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+    return build('gmail', 'v1', credentials=creds)
+
+
+gmail_service = get_gmail_service()
+
+
+# Get list of threads from Gmail
+def get_gmail_thread_ids() -> List[str]:
     try:
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().threads().list(userId='me', maxResults=1).execute()
-        threads_response = get_gmail_threads_response(results)
+        response = gmail_service.users().threads().list(userId='me', maxResults=1).execute()
+        threads = response.get('threads', [])
+        return [thread['id'] for thread in threads]
+    except HttpError as e:
+        print(f"Failed to get a list of threads from Gmail: {e}")
 
-        for thread in threads_response.threads:
-            tdata = service.users().threads().get(userId='me', id=thread['id']).execute()
-            messages = tdata['messages']
-            for message in messages:
-                payload = message.get('payload')
-                body = payload.get('body')
-                parts = payload.get('parts')
-                if body and body.get('size', 0) > 0:
-                    data = body.get('data')
-                    decoded_data = decode_bytes(data)
-                    # print(decoded_data)
-                elif parts:
-                    for part in parts:
-                        body = part.get('body')
-                        if body and body.get('size', 0) > 0:
-                            data = body.get('data')
-                            decoded_data = decode_bytes(data)
-                            # print(decoded_data)
 
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
+def get_gmail_thread(thread_id: str) -> GmailThread:
+    try:
+        response = gmail_service.users().threads().get(userId='me', id=thread_id).execute()
+        return GmailThread(
+            id=response.get('id'),
+            snippet=response.get('snippet'),
+            history_id=response.get('historyId'),
+            messages=response.get('messages', []),
+        )
+    except HttpError as e:
+        print(f"Failed to get thread from Gmail: {e}")
+
+
+# ##### #######
+# EMAIL PARSING
+# ##### #######
+
+def body_has_data(body) -> bool:
+    return body and body.get('size', 0) > 0
+
+
+def is_container_mime_message_part(payload) -> bool:
+    mime_type = payload.get('mimeType')  # type: str
+    return mime_type.startswith('multipart/')
+
+
+def decode_bytes(data):
+    try:
+        return base64.urlsafe_b64decode(data).decode('utf-8')
+    except Exception as e:
+        print(f"An error occurred while decoding: {e}")
+
+
+def decode_body(body) -> str:
+    return decode_bytes(body['data'])
+
+
+def parse_message_part(message_part):
+    if is_container_mime_message_part(message_part):
+        child_message_parts = message_part.get('parts', [])
+        for child_part in child_message_parts:
+            parse_message_part(child_part)
+    else:
+        message_body = message_part.get('body')
+        mime_type = message_part.get('mimeType')
+        if body_has_data(message_body) and mime_type == 'text/plain':
+            decoded_data = decode_body(message_body)
+            print(decoded_data)
+
+
+def parse_thread(thread_id):
+    thread = get_gmail_thread(thread_id)
+    count = 1
+    for message in thread.messages:
+        print(f"Message: {count}")
+        message_part = message.get('payload')
+        parse_message_part(message_part)
+        count += 1
+
+
+def main():
+    thread_ids = get_gmail_thread_ids()
+    for thread_id in thread_ids:
+        parse_thread(thread_id)
 
 
 if __name__ == '__main__':
