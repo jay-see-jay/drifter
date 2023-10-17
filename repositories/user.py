3c7e54fb67ab80c0e1b80438abf03262e32820c3
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-10-17 17:02:23 +0100

    Delete `create_user.py`, refactor `UserRepo` in `repositories/user.py` with added encryption and decryption methods for handling sensitive data, and add the `cryptography` library to `requirements.txt`.

--- repositories/user.py
+++ repositories/user.py
@@ -1,24 +1,40 @@
-# from cryptography.fernet import Fernet
+import os
+
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
 
 from services.database import Database
 from stubs.internal import User
 
+load_dotenv()
+
 
 class UserRepo:
     def __init__(self):
+        key = bytes(os.getenv('SECRET_KEY'), 'utf-8')
         self.db = Database()
+        self.f = Fernet(key)
 
-    def create_user(self, new_user: User):
-        # guest_name = "Brittany"
-        # email = "brittany@yahoo.com"
+    def encrypt(self, text: str):
+        return self.f.encrypt(bytes(text, 'utf-8'))
+
+    def decrypt(self, text: bytes):
+        return self.f.decrypt(text).decode('utf-8')
+
+    def create(self, new_user: User):
+        # Ensure date is a datetime object:
+        # token_expires_at_date = datetime.strptime(os.getenv('TEMP_TOKEN_EXPIRY'), '%Y-%m-%dT%H:%M:%S.%fZ')
         query = """
             INSERT INTO users (email, access_token, refresh_token, token_expires_at, is_active)
             VALUES (%s, %s, %s, %s, %s)
         """
+
+        encrypted_access_token = self.encrypt(new_user.access_token)
+        encrypted_refresh_token = self.encrypt(new_user.refresh_token)
         variables = (
             new_user.email,
-            new_user.access_token,
-            new_user.refresh_token,
+            encrypted_access_token,
+            encrypted_refresh_token,
             new_user.token_expires_at,
             new_user.is_active
         )
@@ -29,7 +45,26 @@ class UserRepo:
 
         self.db.close()
 
-    def delete_user(self, user_id: int):
+    def get(self, email: int):
+        query = """
+            SELECT email, is_active, created_at, access_token, refresh_token, token_expires_at
+            FROM users
+            WHERE email=%s
+        """
+        response = self.db.query(query, (email,))
+        encrypted_access_token = response[0].get('access_token')
+        encrypted_refresh_token = response[0].get('refresh_token')
+        user = User(
+            email=response[0].get('email'),
+            is_active=response[0].get('is_active'),
+            access_token=self.decrypt(encrypted_access_token),
+            refresh_token=self.decrypt(encrypted_refresh_token),
+            token_expires_at=response[0].get('token_expires_at'),
+        )
+        self.db.close()
+        return user
+
+    def delete(self, user_id: int):
         query = """
             DELETE FROM users WHERE pk=%s
         """
