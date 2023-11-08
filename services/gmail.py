12c94213bff89d37696420c8257bfa311dd46b13
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-11-08 10:55:25 +0000

    Start work on processing history

--- services/gmail.py
+++ services/gmail.py
@@ -1,6 +1,7 @@
 import os
 import base64
 import json
+from typing import Dict, Set
 
 from dotenv import load_dotenv
 from email.message import EmailMessage
@@ -12,10 +13,10 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from cloudevents.http import CloudEvent
 
-from utilities.dict_utils import get_value_or_fail
+from utilities.general import get_value_or_fail, process_message_parts
 from utilities.user_utils import *
 from models.user import User
-from repositories import UserRepo, HistoryRepo
+from repositories import *
 from stubs.gmail import *
 from stubs.internal import *
 
@@ -124,72 +125,98 @@ class Gmail:
             'historyId': get_value_or_fail(cloud_event_data, 'historyId'),
         }
 
-    def get_threads(self, page_token=None, count=50) -> GmailThreadsListResponse:
-        response = self.api.users().threads().list(userId='me', pageToken=page_token, maxResults=count).execute()
-        threads = response.get('threads', [])
-        thread_ids = [thread.get('id') for thread in threads]
-        next_page_token = response.get('nextPageToken')
-        return {
-            'thread_ids': thread_ids,
-            'next_page_token': next_page_token
-        }
-
     def get_history(self, start_history_id: str) -> List[History]:
-        history: List[History] = []
+        history_list: List[History] = []
         try:
             response = self.api.users().history().list(userId='me',
                                                        startHistoryId=start_history_id).execute()
-            history.extend(response.get('history', []))
+            history_list.extend(response.get('history', []))
             next_page_token = response.get('nextPageToken')
             while next_page_token:
                 response = self.api.users().history().list(userId='me',
-                                                           next_page_token=next_page_token).execute()
-                history.extend(response.get('history', []))
+                                                           startHistoryId=start_history_id,
+                                                           pageToken=next_page_token).execute()
+                history_list.extend(response.get('history', []))
                 next_page_token = response.get('nextPageToken')
 
-            return history
+            return history_list
 
         except HttpError as e:
             raise e
 
-    def process_history(self, history_list: List[History], user: User):
+    def process_history(self, history_list: List[History]):
         history_repo = HistoryRepo()
-        history_repo.create_many(history_list, user)
-        for history in history_list:
-            self.process_messages_added(history)
-            self.process_messages_deleted(history)
-            self.process_labels_added(history)
-            self.process_labels_removed(history)
-            # Finish by updating `processed_at` column
-            pass
+        # history_repo.create_many(history_list, self.user)
+        # TODO : Add all the messages added in the history_list in one go
+        self.process_messages_added(history_list)
+        # TODO : Mark all the messages that have been deleted
+        # TODO : Ensure all the labels in labels_added and labels_removed are in the db
+        # TODO : For each history record, create a corresponding `messages_labels_history` record
+        # TODO : Finish by updating `processed_at` column
 
-    def process_messages_added(self, history: History):
-        messages_added = history.get('messagesAdded')
-        if not messages_added:
+    def process_messages_added(self, history_list: List[History]):
+        if len(history_list) == 0:
             return
+
+        message_history_ids: Dict[str, str] = dict()
+        for history_record in history_list:  # type: History
+            history_id = history_record.get('id')
+            messages_added = history_record.get('messagesAdded', [])  # type: List[HistoryMessageChanged]
+            for message_added in messages_added:
+                message_id = message_added['message']['id']
+                message_history_ids[message_id] = history_id
+
+        messages = self.get_messages_by_ids(message_history_ids.keys())
+
+        thread_ids: Set[str] = set()
+        for msg_id in messages:
+            msg = messages[msg_id]
+            msg.added_history_id = message_history_ids[msg_id]
+            thread_ids.add(msg.thread_id)
+
+        threads = self.get_threads_by_ids(list(thread_ids))
+
+        thread_repo = ThreadRepo()
+        for t_id in threads:
+            t = threads[t_id]
+            # TODO : Uncomment
+            # thread_repo.upsert(t, self.user)
+
+            for msg in t.messages:  # type: GmailMessage
+                print('Added extra message from new thread')
+                if msg.message_id not in message_history_ids:
+                    messages[msg.message_id] = msg
+
+        # TODO: Create messages
+        message_repo = MessageRepo()
+        message_repo.create_many(messages.values(), self.user)
+        # TODO: Create message_parts
+        # TODO: Create message_headers
+        part_repo = MessagePartRepo()
+        header_repo = HeaderRepo()
+
+        headers, message_parts = process_message_parts(messages)
+        header_repo.create_many(headers, self.user)
+        part_repo.create_many(message_parts, self.user)
+
+        # TODO: Create messages_history
+        # TODO: If no thread, create thread
+        thread_repo.create_many(threads.values(), self.user)
+        # TODO: If no label, create label
+        label_messages: Dict[str, Set[str]] = dict()
+        for msg in messages:
+            for label_id in msg.label_ids:  # type: str
+                if label_id not in label_messages:
+                    label_messages[label_id] = set()
+
+                label_messages[label_id].add(msg.message_id)
+
+        label_ids = [label_id for label_id in label_messages]
+        labels = self.get_labels(label_ids)
+        # TODO: Update messages_labels
+        # TODO: Update messages_labels_history
 
         print('Implement: process_messages_added')
-
-    def process_messages_deleted(self, history: History):
-        messages_deleted = history.get('messagesDeleted')
-        if not messages_deleted:
-            return
-
-        print('Implement: process_messages_deleted')
-
-    def process_labels_added(self, history: History):
-        labels_added = history.get('labelsAdded')
-        if not labels_added:
-            return
-
-        print('Implement: process_labels_added')
-
-    def process_labels_removed(self, history: History):
-        labels_removed = history.get('labelsRemoved')
-        if not labels_removed:
-            return
-
-        print('Implement: process_labels_removed')
 
     def create_batch(self, callback):
         if self.batch is not None or self.batch_request_count != 0:
@@ -210,15 +237,7 @@ class Gmail:
         self.batch_request_count = 0
         self.batch = None
 
-    def get_threads_by_ids(self, thread_ids: List[str], callback):
-        self.create_batch(callback)
-        for thread_id in thread_ids:
-            request = self.api.users().threads().get(userId='me', id=thread_id)
-            self.add_to_batch(request)
-
-        self.finalise_batch()
-
-    def get_thread_by_id(self, thread_id: str) -> GmailThread:
+    def get_thread(self, thread_id: str) -> GmailThread:
         try:
             response = self.api.users().threads().get(userId='me', id=thread_id).execute()
             return GmailThread(
@@ -231,13 +250,120 @@ class Gmail:
         except HttpError as e:
             print(f'Failed to get thread from Gmail: {e}')
 
-    def get_labels(self, label_ids: List[str], callback):
-        self.create_batch(callback)
+    def get_threads(self, page_token=None, count=50) -> GmailThreadsListResponse:
+        response = self.api.users().threads().list(userId='me', pageToken=page_token, maxResults=count).execute()
+        threads = response.get('threads', [])
+        thread_ids = [thread.get('id') for thread in threads]
+        next_page_token = response.get('nextPageToken')
+        return {
+            'thread_ids': thread_ids,
+            'next_page_token': next_page_token
+        }
+
+    def get_threads_by_ids(self, thread_ids: List[str]) -> Dict[str, GmailThread]:
+        threads: Dict[str, GmailThread] = dict()
+
+        if len(thread_ids) == 0:
+            return threads
+
+        def process_thread_response(response_id: str, response: dict, exception):
+            if exception is not None:
+                print(f'Response ID: {response_id} - failed to get thread.\n{exception}')
+            else:
+                threads[response['id']] = GmailThread(
+                    thread_id=response.get('id'),
+                    history_id=response.get('historyId'),
+                    messages=response.get('messages'),
+                    snippet=response.get('snippet'),
+                )
+
+        self.create_batch(process_thread_response)
+        for thread_id in thread_ids:
+            request = self.api.users().threads().get(userId='me', id=thread_id)
+            self.add_to_batch(request)
+
+        self.finalise_batch()
+
+        return threads
+
+    def get_messages_by_ids(self, message_ids: List[str]) -> Dict[str, GmailMessage]:
+        messages: Dict[str, GmailMessage] = dict()
+
+        if len(message_ids) == 0:
+            return messages
+
+        def process_message_response(response_id: str, response: dict, exception):
+            if exception is not None:
+                print(f'Response ID: {response_id} - failed to get message.\n{exception}')
+            else:
+                msg_id = response.get('id')
+                messages[msg_id] = GmailMessage(
+                    message_id=msg_id,
+                    thread_id=response.get('threadId'),
+                    label_ids=response.get('labelIds'),
+                    snippet=response.get('snippet'),
+                    history_id=response.get('historyId'),
+                    internal_date=response.get('internalDate'),
+                    payload=response.get('payload'),
+                    size_estimate=response.get('sizeEstimate'),
+                )
+
+        self.create_batch(process_message_response)
+        for message_id in message_ids:
+            request = self.api.users().messages().get(userId='me', id=message_id)
+            self.add_to_batch(request)
+
+        self.finalise_batch()
+
+        return messages
+
+    def get_message_by_id(self, message_id: str) -> GmailMessage:
+        try:
+            response = self.api.users().messages().get(userId='me', id=message_id).execute()
+            return GmailMessage(
+                message_id=response.get('id'),
+                thread_id=response.get('threadId'),
+                label_ids=response.get('labelIds'),
+                snippet=response.get('snippet'),
+                history_id=response.get('historyId'),
+                internal_date=response.get('internalDate'),
+                payload=response.get('payload'),
+                size_estimate=response.get('sizeEstimate'),
+            )
+        except HttpError as e:
+            print(f'Failed to get message from Gmail: {e}')
+
+    def get_labels(self, label_ids: List[str]) -> List[GmailLabel]:
+        labels: List[GmailLabel] = []
+
+        if len(label_ids) == 0:
+            return labels
+
+        def process_label_response(response_id: str, response: dict, exception):
+            if exception is not None:
+                print(f'Response ID: {response_id} - failed to get label.\n{exception}')
+            else:
+                labels.append(GmailLabel(
+                    label_id=response.get('id'),
+                    name=response.get('name'),
+                    label_type=response.get('type'),
+                    messages_total=response.get('messagesTotal'),
+                    messages_unread=response.get('messagesUnread'),
+                    threads_total=response.get('threadsTotal'),
+                    threads_unread=response.get('threadsUnread'),
+                    color=response.get('color'),
+                    message_list_visibility=response.get('messageListVisibility'),
+                    label_list_visibility=response.get('labelListVisibility')
+                ))
+
+        self.create_batch(process_label_response)
         for label_id in label_ids:
             request = self.api.users().labels().get(userId='me', id=label_id)
             self.add_to_batch(request)
 
         self.finalise_batch()
+
+        return labels
 
     @staticmethod
     def parse_message_headers(message_part: GmailMessagePart) -> ParsedMessageHeaders:
@@ -300,7 +426,7 @@ class Gmail:
         message['To'] = recipient
         message['From'] = get_my_email()
 
-        # encoded message
+        # encoded messageg
         encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
         create_message = {
