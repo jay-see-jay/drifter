1979200ab43bbae1183f4e1d60b922509108792b
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-11-08 10:53:49 +0000

    Move gmail batch request callback into self contained function

--- functions/sync_gmail_mailbox.py
+++ functions/sync_gmail_mailbox.py
@@ -6,6 +6,7 @@ from services.gmail import Gmail
 from repositories import *
 from models.user import User
 from stubs.gmail import *
+from utilities.general import process_message_part
 
 
 def handle_sync_gmail_mailbox(request: Request) -> Response:
@@ -22,23 +23,12 @@ def handle_sync_gmail_mailbox(request: Request) -> Response:
     page_token = None
     threads: Dict[str, GmailThread] = dict()
 
-    def process_thread_response(response_id: str, response: dict, exception):
-        if exception is not None:
-            print(f'Response ID: {response_id} - failed to get thread.\n{exception}')
-        else:
-            threads[response['id']] = GmailThread(
-                thread_id=response.get('id'),
-                history_id=response.get('historyId'),
-                messages=response.get('messages'),
-                snippet=response.get('snippet'),
-            )
-
     # Can maybe reply to this to explain batching requests to Gmail API?
     # https://stackoverflow.com/questions/26004335/get-multiple-threads-by-threadid-in-google-apps-scripts-gmailapp-class
     while True:
         threads_list_response = gmail.get_threads(page_token=page_token)
         thread_ids = threads_list_response['thread_ids']
-        gmail.get_threads_by_ids(thread_ids, process_thread_response)
+        threads.update(gmail.get_threads_by_ids(thread_ids))
         next_page_token = threads_list_response.get('nextPageToken', None)
         if next_page_token:
             page_token = next_page_token
@@ -48,25 +38,13 @@ def handle_sync_gmail_mailbox(request: Request) -> Response:
     label_messages: Dict[str, Set[str]] = dict()
     message_parts: List[GmailMessagePart] = []
     headers: List[GmailHeader] = []
-
-    def process_message_part(
-        part: GmailMessagePart,
-        parent_message_id: str,
-    ):
-        headers.extend(part.headers)
-        message_parts.append(part)
-        child_parts = part.parts
-        if child_parts:
-            for child_part in child_parts:
-                process_message_part(child_part, parent_message_id)
-
     messages: List[GmailMessage] = []
-    for thread_id in threads:
-        thread = threads.get(thread_id)  # type: GmailThread
 
-        messages.extend(thread.messages)
+    for t_id in threads:  # type: GmailThread
+        t = threads[t_id]
+        messages.extend(t.messages)
 
-        for msg in thread.messages:  # type: GmailMessage
+        for msg in t.messages:  # type: GmailMessage
             # Process labels
             for label_id in msg.label_ids:  # type: str
                 if label_id not in label_messages:
@@ -75,29 +53,12 @@ def handle_sync_gmail_mailbox(request: Request) -> Response:
                 label_messages[label_id].add(msg.message_id)
 
             # Process payload / message part(s)
-            process_message_part(msg.payload, msg.message_id)
+            part_headers, part_child_parts = process_message_part(msg.payload)
+            headers.extend(part_headers)
+            message_parts.extend(part_child_parts)
 
     label_ids = [label_id for label_id in label_messages]
-    labels: List[GmailLabel] = []
-
-    def process_label_response(response_id: str, response: dict, exception):
-        if exception is not None:
-            print(f'Response ID: {response_id} - failed to get label.\n{exception}')
-        else:
-            labels.append(GmailLabel(
-                label_id=response.get('id'),
-                name=response.get('name'),
-                label_type=response.get('type'),
-                messages_total=response.get('messagesTotal'),
-                messages_unread=response.get('messagesUnread'),
-                threads_total=response.get('threadsTotal'),
-                threads_unread=response.get('threadsUnread'),
-                color=response.get('color'),
-                message_list_visibility=response.get('messageListVisibility'),
-                label_list_visibility=response.get('labelListVisibility')
-            ))
-
-    gmail.get_labels(label_ids=label_ids, callback=process_label_response)
+    labels = gmail.get_labels(label_ids=label_ids)
 
     thread_repo = ThreadRepo()
     thread_repo.create_many(threads.values(), user)
