2f010353678467a1bb7e0623e3467c536db7dedc
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-11-08 10:54:57 +0000

    Add upsert and update_history_id values in thread repo

--- repositories/thread.py
+++ repositories/thread.py
@@ -10,7 +10,37 @@ class ThreadRepo:
     def __init__(self):
         self.db = Database()
 
+    def upsert(self, thread: GmailThread, user: User):
+        threads_exists = self.exists(thread.thread_id)
+        if threads_exists:
+            self.update_history_id(thread)
+        else:
+            self.create_many([thread], user)
+
+    def update_history_id(self, thread: GmailThread):
+        query = 'UPDATE threads SET history_id="%s" WHERE thread_id="%s"'
+        variables = (thread.history_id, thread.thread_id)
+        try:
+            self.db.query(query, variables)
+        except mysql.connector.Error as e:
+            print(f'Failed to update history_id of thread: {e}')
+
+    def exists(self, thread_id: str) -> bool:
+        query = 'SELECT id FROM threads WHERE id="%s"'
+        variables = (thread_id,)
+        try:
+            response = self.db.query(query, variables)
+            response_thread_id = response[0].get('id')
+            return response_thread_id == thread_id
+        except mysql.connector.Error as e:
+            if e.msg == 'Not found':
+                return False
+            print(f'Could not verify that thread was in db: {e}')
+
     def create_many(self, threads: List[GmailThread], user: User):
+        if len(threads) == 0:
+            return
+
         columns = [
             'id',
             'snippet',
