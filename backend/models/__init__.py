ef996e0766f11050856c4fdcf7d30abac88debdc
jay-see-jay <6587212+jay-see-jay@users.noreply.github.com>
2023-11-24 16:50:17 +0000

    Export models from init

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,1 +1,5 @@
 from models.user import User
+from models.message_part import MessagePart
+from models.message import Message, MessageCreateVariablesType
+from models.draft import *
+from models.thread import *
